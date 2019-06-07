6263d857fd8b8683194b32c167055ce59c819a4e
Shikeless <Shikeless@gmail.com>
2019-06-07 20:07:56 +0300

    adding new lessons

--- asynchrony/index.js
+++ asynchrony/index.js
@@ -35,3 +35,63 @@ const queue = [                    //визуализация SetTimeout
 		start: 123141421
 	}
 ];
+
+
+//PROMISE
+
+const promise = {
+	state: ['pending',    //ожидание
+			'fulfiled',   //выполнено успешно
+			'rejected'	  //выполнено неудачно
+	],
+	resolvedQueue: [      //очередь функций выполняемая в момент перехода promise от pending в fulfiled
+		function() {
+			console.log('1');
+		},
+		function() {
+			console.log('2');
+		},
+		function() {
+			console.log('3');
+		}
+	],
+	rejectedQueue: [      //очередь функций выполняемая в момент перехода promise от pending в rejected
+		function() {
+			console.log('1');
+		},
+		function() {
+			console.log('2');
+		},
+		function() {
+			console.log('3');
+		}
+	]
+}
+
+console.log('до promise');
+const tmp = new Promise(function(resolve, reject) { //resolve - переводчик в режим успешного выполнения //reject - выполнено неудачно
+	resolve();
+});
+console.log('после promise');
+
+function delay(ms) {
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve();                             
+			console.log('resolved');
+		}, ms)
+	})
+}
+
+const prom = delay(2000);
+
+console.log('До then');
+prom.then(() => console.log(1));           //добавляет новую функцию в очередь resolvedQueue:
+prom.then(() => console.log(2));  		   //и создаем новый объект promise с пустым resolveQueue:
+prom.then(() => console.log(3));
+console.log('После then');  			   //then только добавляет новые параметры в объект prom, который 
+										   //вызывает функцию delay с задержжкой в 2 секунды, по этому сначала
+										   //отработают консоль логи
+
+
+//ASYNC AWAIT
